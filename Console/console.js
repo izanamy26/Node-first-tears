0c65402319aae02a4bca5a30acbd93dd3990e33c
Nastya <avy-w@yandex.ru>
2019-10-09 23:17:18 +0300

    Нарисованы прямоугольники по отдельности, но ...

--- Console/console.js
+++ Console/console.js
@@ -39,10 +39,60 @@ fs.readFile('instructions.txt', function (err, logData) {
 	if (err) throw err;
 
     var text = logData.toString().split("\r\n");
+
+    console.log(text);
+
     let painting = text.reduce((prev, item) => {
-        let figure = item.split();
-    }, []);
+        let [figure, x, y, ...params] = item.split(' ');
     
-    console.log('\x1b[46m', 'I am cyan');  //cyan
+         switch (figure) {
+             case 'rect':
+                 let [width, height, color, filled] = params;
+                 getRect({figure, x, y, width, height, color, filled});
+                 break;
+         }   
+
+
+
+
+        return [];
+    }, []);
+
+    //console.log(painting);
+
+    //console.log('\x1b[46m', 'I am cyan');  //cyan
 
 });
+
+function getRect(params) {
+    console.log(params);
+
+    let x = Number(params.x);
+    let y = Number(params.y);
+    let width = Number(params.width);
+    let height = Number(params.height);
+
+    let canvas = [];
+
+    for (let i = 0; i <= y + height; i++) {
+       let row = Array(width + x ).fill(' ');
+
+       if (i >= y && i <= y + height) {
+           if (params.filled === 'filled') {
+            row.fill('*', x, x + width);
+           } else {
+            row.fill('*', x, x + 1);
+            row.fill('*', x + width - 1,  x + width );
+           }
+       }
+
+       if ((i == y || i == y + height) && params.filled === undefined) {
+            row.fill('*', x, x + width);
+       }
+       
+        canvas.push(row);
+    }
+
+    console.log('canvas: ');
+    console.log(canvas);
+}
