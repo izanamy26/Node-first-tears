d4a722fcd07bd10024024722a2a760ecafc7ec76
Sora <avy-w@yandex.ru>
2019-11-03 22:42:17 +0300

    get-post

---
+++ Console/drawer.js
@@ -0,0 +1,192 @@
+const fs = require('fs');
+
+/*const colors = {
+    Reset: "\x1b[0m",
+    Bright: "\x1b[1m",
+    Dim: "\x1b[2m",
+    Underscore: "\x1b[4m",
+    Blink: "\x1b[5m",
+    Reverse: "\x1b[7m",
+    Hidden: "\x1b[8m",
+    fg: {
+     Black: "\x1b[30m",
+     Red: "\x1b[31m",
+     Green: "\x1b[32m",
+     Yellow: "\x1b[33m",
+     Blue: "\x1b[34m",
+     Magenta: "\x1b[35m",
+     Cyan: "\x1b[36m",
+     White: "\x1b[37m",
+     Crimson: "\x1b[38m" //القرمزي
+    },
+    bg: {
+     Black: "\x1b[40m",
+     Red: "\x1b[41m",
+     Green: "\x1b[42m",
+     Yellow: "\x1b[43m",
+     Blue: "\x1b[44m",
+     Magenta: "\x1b[45m",
+     Cyan: "\x1b[46m",
+     White: "\x1b[47m",
+     Crimson: "\x1b[48m"
+    }
+   };
+   console.log(colors.bg.Blue, colors.fg.White , "I am white message with blue background", colors.Reset) ;*/
+
+const Drawer = {
+    readFile: (file) => {
+       return new Promise((resolve, reject) => {      // Promise { <pending> }
+           fs.readFile( file, "utf8", (err, logData) => {   
+                if (err) {
+                    reject(err);
+                } 
+
+                resolve(()=> {
+                    let text = logData.split("\n");
+
+                    // console.log('figure: ', figure, '  ', Drawer.handlers[figure]);    
+
+                    // if (Drawer.handlers[figure] === undefined) {
+                    //     throw "Can't find figure " + figure;
+                    // }   
+
+            
+                    let figures = text.map((item) => {
+                        let [figure, ...params] = item.split(' '); 
+
+                        return Drawer.handlers[figure](params);
+                    });
+            
+                let canvas = joinCanvases(figures);     
+                
+                return canvas;
+                });
+            });
+       });
+    },
+
+    handlers: {
+        rect: ([x, y, width, height, colorRect, filled]) => {
+            x = Number(x);
+            y = Number(y);
+            width = Number(width);
+            height = Number(height);
+            return getRect({ x, y, width, height, colorRect, filled });
+        },
+    
+        line: ([x, y, length, position, colorLine]) => {
+            x = Number(x);
+            y = Number(y);
+            length = Number(length);
+            return getLine({ x, y, length, position, colorLine });
+        },
+    
+        text: ([x, y, colorText, ...text]) => {
+            x = Number(x);
+            y = Number(y);
+            text = text.join(' ');
+            return getText({ x, y, text, colorText });
+        }
+    }
+};   
+
+
+const getRect = ({ x, y, width, height, color, filled }) => {
+    let canvas = [];
+
+    for (let i = 0; i <= y + height; i++) {
+       let row = Array(width + x ).fill(' ');
+
+       if (i >= y && i <= y + height) {
+           if (filled === 'filled') {
+            row.fill('*', x, x + width);
+           } else {
+            row.fill('*', x, x + 1);
+            row.fill('*', x + width - 1,  x + width );
+           }
+       }
+
+       if ((i == y || i == y + height) && filled === undefined) {
+            row.fill('*', x, x + width);
+       }
+       
+        canvas.push(row);
+    }
+
+    return canvas;
+}
+
+const getLine = ({ x, y, length, position, color }) => {
+        let canvas = [];
+
+        switch(position) {
+            case 'row':
+                for (let i = 0; i <= y; i++) {
+                    let row = Array(x + length).fill(' ');
+
+                    if (i == y) {
+                        row.fill('-', x,  x + length);     
+                    }
+                    
+                    canvas.push(row);
+                }
+                break;
+
+            case 'column':
+                    for (let i = 0; i <= y + length; i++) {
+                        let row = Array(x + 1).fill(' ');
+    
+                        if (i >= y) {
+                            row.fill('|', x,  x + 1);     
+                        }
+                        canvas.push(row);
+                    }
+                break;    
+        }
+
+    return canvas;
+}
+
+const getText = ({x, y, text, color}) => {
+    let canvas = [];
+     text = text.split('');
+
+    for (let i = 0; i <= y; i++) {
+        let row = Array(x + text.length).fill(' ');
+
+        if (i == y) {
+            row = Array(x).fill(' ', 0 , x)
+            row.push(...text);     
+        }
+        
+        canvas.push(row);
+    }
+
+    return canvas;
+} 
+
+
+const joinCanvases = (figures) => {
+    var canvas = [];
+
+    for (let i = 0; i < figures.length; i++) {
+        let figure = figures[i];
+        for (let y = 0; y < figure.length; y++) {
+            if (canvas[y] === undefined) {
+                canvas[y] = [];     
+            }
+            
+            for (let x = 0; x < figure[y].length; x++) {
+               if (canvas[y][x] !== undefined && canvas[y][x] !== ' ' && figure[y][x] === ' ') {
+                    continue;
+               }
+
+                canvas[y][x] = figure[y][x]; 
+            }
+        }
+    }
+
+    return canvas;
+}
+
+module.exports = Drawer;
--- Console/index.js
+++ Console/index.js
@@ -1,3 +1,70 @@
-const drawer = require('./console.js');
+const drawer = require('./drawer.js');
+const http = require('http');
+const express = require('express');
+const fs = require('fs');
 
-drawer.readFile('instructions.txt').then((res) => console.log(res()));
+const app = express();
+
+/* drawer.readFile('instructions.txt').then((res) => console.log(res()));
+ */
+
+const jsonParser = express.json();
+
+//  curl -H "Content-Type: application/json" -H "iv-user: nastya" -X POST -d '{"user":"nastya","pass":"888"}' http://localhost:3000/cimages
+app.post('/cimages', jsonParser, (request, response)=> {
+    if(!request.body) 
+        return response.sendStatus(400);
+    
+    const text = request.body;
+    const user = request.headers['iv-user'];     
+    const guid = getGuid();
+
+    response.send(guid);
+    let path = './storage/' + guid + '.cimage';
+
+    fs.writeFile(path, JSON.stringify({user, text, guid}), 'utf8', (err) => {
+        console.log('Error: ', err);
+    });
+});
+
+
+app.get('/cimages/:id', (request, response) => {
+    const id = request.params['id'];
+
+    let path = './storage/' + id + '.cimage';
+    let flag = request.query.image !== undefined;
+
+    console.log('flag: ', flag);
+
+    fs.access(path, (err) => {
+        if (err) {
+            response.sendStatus(404);
+        }
+    });
+
+
+    if (flag) {
+        drawer.readFile(path)
+            .then((result) => response.send(result));
+    } else {
+        fs.readFile(path, "utf8", (err, data) => {
+            if (err) {
+    
+            }
+    
+            response.send(data);      
+        });
+    }
+});
+
+
+app.listen(3000);
+
+const getGuid = () => {
+    return [
+        (Math.random().toString(16).substr(2) + '0'.repeat(12)).substr(0, 12),
+        (Math.random().toString(16).substr(2) + '0'.repeat(12)).substr(0, 12),
+        (Math.random().toString(16).substr(2, 8) + '0'.repeat(8)).substr(0, 8)
+    ].join('-').toUpperCase();
+};
+
